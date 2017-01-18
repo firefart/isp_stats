6bceb68fe9f979dfd78b331e0ae0b20cfa2bcede
Christian Mehlmauer <firefart@gmail.com>
2017-01-19 00:29:00 +0100

    data structure

--- stats.py
+++ stats.py
@@ -27,23 +27,30 @@ except ValueError as e:
 
 data = []
 
+# sample data
+# server": {"latency": 17.473, "name": "Vienna", "url": "http://speedtest.nessus.at/speedtest/upload.php", "country": "Austria", "lon": "16.3726", "cc": "AT", "host": "speedtest.nessus.at:8080", "sponsor": "Nessus GmbH (10G Uplink)", "url2": "http://speedtest2.nessus.at/speedtest/upload.php", "lat": "48.2088", "id": "3744", "d": 1.3292411343362884}}
+
 if j:
     data.append({
-        "measurement": "download_speed",
+        "measurement": "speedtest",
+        "tags": {
+            "server_id": j["server"]["id"],
+            "server_name": j["server"]["name"],
+            "server_host": j["server"]["host"],
+            "server_sponsor": j["server"]["sponsor"],
+            "server_latency": j["server"]["latency"],
+            "server_url": j["server"]["url"],
+            "server_url2": j["server"]["url2"],
+            "server_country": j["server"]["country"],
+            "server_cc": j["server"]["cc"],
+            "server_lon": j["server"]["lon"],
+            "server_lat": j["server"]["lat"],
+            "server_d": j["server"]["d"]
+        },
         "fields": {
-            "speed": j["download"]
-        }
-    })
-    data.append({
-        "measurement": "upload_speed",
-        "fields": {
-            "speed": j["upload"]
-        }
-    })
-    data.append({
-        "measurement": "ping",
-        "fields": {
-            "latency": j["ping"]
+            "download_speed": j["download"],
+            "upload_speed": j["upload"],
+            "ping": j["ping"]
         }
     })
 
